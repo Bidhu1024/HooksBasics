ce68b72fea13fc5cc37613230cb75fefa61c771c
Bidhu1024 <bidhu1024av@gmail.com>
2023-09-07 21:58:16 +0530

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
+import {useState, useMemo} from "react"
 import './App.css';
 
 function App() {
+  const [dark,setDark] = useState(false)
+const [number,setNumber]=useState(0)
+
+  let backStyle = {
+    backgroundColor: dark ? "#121416" : "white",
+    color:dark?"white":"dark"
+  }
+
+  const slowFunc= (num)=>{
+    console.log("start")
+    for(let i =0;i<1000; i++){}
+    return num
+  }
+  const doubleNum = useMemo((number)=>{
+    slowFunc(number)
+  },[number])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style = {backStyle}>
+    <input type="number" value = {number} onChange={(e)=>{setNumber(parseInt(e.target.value))}} />
+     <button onClick={()=>setDark(prev => !prev)}> Click me</button>
+     <div>
+      {doubleNum}
+     </div>
     </div>
   );
 }
